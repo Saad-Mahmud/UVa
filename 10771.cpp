3663a9392f604bea1452d6dc34e61d49acb942af
Saad-Mahmud <saadmahmud14@gmail.com>
2018-01-04 21:54:44 +0600

    Create 10771.cpp

---
+++ 10771.cpp
@@ -0,0 +1,24 @@
+//2k18//Q1GD-1//
+//10771 - Barbarian tribes
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i,L,R) for(int i=(L);i<=(R);i++)
+#define rept(i,L,R) for(int i=(L);i<(R);i++)
+#define isc(n) scanf("%d",&n)
+#define llsc(n) scanf("%lld",&n)
+#define dsc(n) scanf("%lf",&n)
+#define enl cout<<endl
+#define PB(x) push_back(x)
+#define MP(x,y) make_pair(x,y)
+#define xx first
+#define yy second
+typedef long long ll;
+typedef pair<int,int>PI;
+typedef pair<pair<int,int>,int>PII;
+ 
+int main()
+{  
+    int n,m,k;  
+    while(scanf("%d %d %d",&n,&m,&k)!=EOF && n+k+m>0)(m&1)?puts("Keka"):puts("Gared"); 
+    return 0;  
+}
